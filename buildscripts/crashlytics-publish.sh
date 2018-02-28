2928505a6d6133c9083f9da9163d0c0dce332100
Eric Glaenzer <ericg@socketmobile.com>
2018-02-28 11:48:51 -0500

    trying to detect if the build happens on the master branch

--- buildscripts/crashlytics-publish.sh
+++ buildscripts/crashlytics-publish.sh
@@ -15,15 +15,15 @@ fi
 
 echo publish Capture NuGet for Windows
 # branch="$(git branch | head -n 1)"
-branch="$(git symbolic-ref HEAD 2>/dev/null)"
-master=`git rev-parse --short origin/master`
-echo "branch: " $branch
-echo "master: " $master
+# branch="$(git symbolic-ref HEAD 2>/dev/null)"
+branch=`git rev-parse --short HEAD`
+master=`git rev-parse --short master`
+echo "hash current branch: " $branch
+echo "hash for master: " $master
 # if "%BRANCH%"=="* (HEAD detached at %MASTER%)" (
 #   echo push Capture NuGet for Windows
 #   nuget push "%buildscripts%\SocketMobile.Capture.*.nupkg" -s http://nwkgit1.socketcomm.com/nuget/ 577FB113A50342CCBCB67A5A90264923
 # )
-
 #${SOURCE_ROOT}/Crashlytics.framework/submit  <API_KEY> \
 # <BUILD_SECRET> \
 # -emails $(lastcommitEmail) \
@@ -31,7 +31,12 @@ echo "master: " $master
 # -groupAliases GroupAlias,GroupAlias2 \
 # -notifications YES
 
+if [ "$branch" == "$master" ]
+then
 LastCommitEmail=`git show --format="%aE" | head -n 1`
 echo "send a notification to " $LastCommitEmail
 echo "for this file " $1StockCount/SockCount.ipa
 # $1../Pods/Crashlytics/submit $CrashlyticsApiKey $CrashlyticsSecret -ipaPath $1StockCount/StockCount.ipa -emails $LastCommitEmail -notifications YES
+else
+	echo "not on master branch so nothing gets published through Crashlytics"
+fi
