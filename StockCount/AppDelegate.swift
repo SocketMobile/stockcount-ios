b09c772b8c92e766a0dc1ab98cdb70a317f58a36
panlong90 <long@socketmobile.com>
2018-04-13 15:56:57 -0700

    update developer id and AppKey

--- StockCount/AppDelegate.swift
+++ StockCount/AppDelegate.swift
@@ -24,10 +24,10 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         
         // Initialize CaptureHelper
         let AppInfo = SKTAppInfo()
-        AppInfo.appKey="MC4CFQDdEFdODdiceD4YGANGb3Q/MK9U0AIVAMUpLrTNqBkOlpMpLd2GATJuSt47"
+    AppInfo.appKey="MCwCFDqSvrhLenQ9qu49ADSL3K+9mWV8AhQIamqcj1fjbGZVcJ3r+t5ijm+/ow=="
         AppInfo.bundleID="ios:com.socketmobile.stockCount"
 
-        AppInfo.developerID="EF62BC15-59E0-4E86-82A3-493101D7DB4E"
+        AppInfo.developerID="bb57d8e1-f911-47ba-b510-693be162686a"
         
         let captureHelper = CaptureHelper.sharedInstance
         captureHelper.delegateDispatchQueue = DispatchQueue.main
