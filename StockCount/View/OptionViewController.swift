655e38716f7e4501545c90f2654449b9866d7954
shawn <paramountstarllc@gmail.com>
2019-04-22 15:34:33 -0700

    - updated buy scanner link for multi region

--- StockCount/View/OptionViewController.swift
+++ StockCount/View/OptionViewController.swift
@@ -45,6 +45,8 @@ class OptionViewController : UIViewController {
         txtLink.attributedText = attributedString
         txtLink.textAlignment = NSTextAlignment.center
         txtLink.delegate = self
+        
+        loadingIndicator.isHidden = true
     }
     
     @IBAction func onBtnGetStarted(_ sender: Any) {
@@ -58,22 +60,28 @@ class OptionViewController : UIViewController {
     func updateVersion(_ appVer: String, _ captureVer: String) {
         lblVersion.text = "\("version".localized)\(appVer)\n\("Capture".localized) \(captureVer)"
     }
+    
     @IBAction func onBuyScannerClicked(_ sender: Any) {
-        let pub_key = "pub_9a42760dc57269d2f616"
-        let sec_key = "sec_86be30771c1af32a57df"
-        let aff_code = "7daad6"
+        let regionCode = Locale.current.regionCode
         
+        if regionCode == "CN" {
+            UIApplication.shared.open(URL(string: ChinaLink)!, options: [:], completionHandler: nil)
+            return
+        }
+        
+        let refersionKey = getRefersionKeys(regionCode!)
         let parameters: Parameters = [
-            "refersion_public_key": pub_key,
-            "refersion_secret_key": sec_key,
-            "affiliate_code": aff_code
+            "refersion_public_key": refersionKey.pub_key,
+            "refersion_secret_key": refersionKey.sec_key,
+            "affiliate_code": refersionKey.aff_code
         ]
-        
         let url = "https://www.refersion.com/api/get_affiliate"
         loadingIndicator.isHidden = false
+        loadingIndicator.startAnimating()
         
         request(url, method: .post, parameters: parameters, encoding: JSONEncoding.default).responseJSON { (resp) in
             self.loadingIndicator.isHidden = true
+            self.loadingIndicator.stopAnimating()
             if let _ = resp.error {
                 let alertController = UIAlertController(title: "error".localized, message: resp.error?.localizedDescription ?? "", preferredStyle: .alert)
                 alertController.addAction(UIAlertAction(title: "ok".localized, style: .default, handler: nil))
